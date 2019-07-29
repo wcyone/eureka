24d725af7ac590845459cf18c63c12e46b60132e
wcyone <421035835@qq.com>
2019-07-29 15:51:22 +0800

    修改输入参数

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -7,6 +7,7 @@ package controller;
  */
 public class UserController {
     public static void main(String[] args){
-       System.out.print("测试分支合并");
+        Integer m = 12;
+       System.out.print(m);
     }
 }
