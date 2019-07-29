e19a32897f846c76f77cb6c69b1f68ad9f46749e
wcyone <421035835@qq.com>
2019-07-29 15:44:39 +0800

    测试分支合并

---
+++ src/main/java/controller/UserController.java
@@ -0,0 +1,12 @@
+package controller;
+
+/**
+ * @author wcy
+ * @date 2019/7/29 15:43
+ * @Description:
+ */
+public class UserController {
+    public static void main(String[] args){
+       System.out.print("测试分支合并");
+    }
+}
